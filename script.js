68ab29e45952b804bd3f94410848d5bf00e44545
fedsza <61564582+fedsza@users.noreply.github.com>
2020-03-05 00:52:53 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var balance = 0;
-window.onload = function() {
-  var x = document.getElementById("butt");
-  x.onclick = function() {
-    balance++;
-    var elem = document.getElementById("first");
-    elem.innerHTML = balance;
-  }
-};
